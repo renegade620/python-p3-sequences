c9537bb40a81734bd38707bf24dd9fd9a33c6bf8
Frankline Were <franklinegift@gmail.com>
2024-08-30 11:22:51 +0300

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fib_list = [0, 1]
+    
+    if length == 1:
+        print([0])
+        return
+    
+    for i in range(2, length):
+        fib_list.append(fib_list[-1] + fib_list[-2])
+    
+    print(fib_list[:length])
